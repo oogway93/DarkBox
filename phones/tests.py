89b3aeaef5797ca711c22dcc1811c362abdc43e9
oogway93 <lemeshinskiym@gmail.com>
2023-08-07 20:54:24 +0300

    tests for base.html page

--- phones/tests.py
+++ phones/tests.py
@@ -1,1 +1,10 @@
-# from django.test import TestCase
+from django.test import TestCase
+
+
+class HomePageTest(TestCase):
+    def test_base_page(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTrue(html.endswith("</html>"))
+        self.assertIn("<hr>", html)
+        self.assertTemplateUsed(response, 'base.html')
